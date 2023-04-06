5c92495d719f0e618269376e5776950fd040d8cc
Thiago Pinheiro <thiagopdsilva1@gmail.com>
2023-04-06 20:42:34 -0300

    Projeto criado

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,90 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long Id;
+	private String name;
+	private String phone;
+	private String email;
+	private String password;
+	
+	public User() {
+		
+	}
+
+	public User(Long id, String name, String phone, String email, String password) {
+		super();
+		Id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(Id, other.Id);
+	}
+	
+	
+}
